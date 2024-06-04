2470ac1d34283e610ee9c57aa4479130568a686e
thisang <sang.ho25@student.passerellesnumeriques.org>
2024-06-04 22:16:49 +0700

    feat: config source code

---
+++ src/components/TdTable.js
@@ -0,0 +1,42 @@
+function TdTable({ props }) {
+  return (
+    <tr>
+      <td>
+        <strong>{props.id}</strong>
+      </td>
+      <td>{props.item1}</td>
+      <td>{props.item2}</td>
+      <td>{props.item3}</td>
+      <td>{props.item4}</td>
+      <td>{props.item5}</td>
+      <td>
+        <div className="dropdown">
+          <button
+            type="button"
+            className="btn p-10 dropdown-toggle hide-arrow"
+            data-bs-toggle="dropdown"
+          >
+            <i
+              className="fa-solid fa-ellipsis-vertical"
+            ></i>
+          </button>
+          <div className="dropdown-menu">
+            <a
+              className="dropdown-item"
+              href="#!"
+            >
+              <i className="fa-solid fa-pen"></i> Edit
+            </a>
+            <a
+              className="dropdown-item"
+              href="#!"
+            >
+              <i className="fa-solid fa-trash" ></i>
+              Delete
+            </a>
+          </div>
+        </div>
+      </td>
+    </tr>
+  );
+}
---
+++ src/pages/Order/index.js
@@ -0,0 +1,34 @@
+function Order() {
+  return (
+    <div class="content-wrapper">
+      <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="card">
+          <h5 class="card-header">Users</h5>
+          <div class="d-flex justify-content">
+            <a class="btn btn-primary " href="#!" id="">
+              Create new
+            </a>
+          </div>
+          <div class="table-responsive text-nowrap">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>User name</th>
+                  <th>Phone</th>
+                  <th>Email</th>
+                  <th>Date of birth</th>
+                  <th>Address</th>
+                  <th>Avatar</th>
+                  <th>Role</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody class="table-border-bottom-0"></tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
