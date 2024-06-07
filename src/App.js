bcc9b9e701cfb4f6b42319b778d7bb1896a82275
Linh <nguyenthilinhqni2020@gmail.com>
2024-06-07 21:20:35 +0700

    feat: handle api show category refer to ST-224

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {  Routes, Route, Navigate } from "react-router-dom";
-import { Order, Blog, CreateBlog, ShowContact, UpdateContact, UpdateBlog , Product} from "./pages";
+import { Order, Blog, CreateBlog, ShowContact, UpdateContact, UpdateBlog , Product,ShowCategory} from "./pages";
 import Login from "./pages/Login";
 import React from "react";
 
@@ -16,6 +16,7 @@ function App() {
         <Route path="/update-contact" element={<UpdateContact />} />
         <Route path="/update-blog/:id" element={<UpdateBlog />} />
         <Route path="/products" element={<Product />} />
+        <Route path="/categories" element={<ShowCategory />} />
       </Routes>
 
   );
---
+++ src/pages/Category/index.js
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import axiosService from "../../services/configAxios";
+
+function ShowCategory() {
+  const [categories, setCategory] = useState([]);
+  const [openedMenuIndex, setOpenedMenuIndex] = useState(null);
+
+  const toggleMenu = (index) => {
+    setOpenedMenuIndex(index === openedMenuIndex ? null : index);
+  };
+  const getCategory = async () => {
+    try {
+      const response = await axiosService.get("/categories");
+      setCategory(response.data.data);
+    } catch (error) {
+      console.log("Error get all category", error);
+    }
+  };
+  useEffect(() => {
+    getCategory();
+  }, []);
+  return (
+    <>
+      <div className="content-wrapper">
+        <div className="container-xxl flex-grow-1 container-p-y">
+          <div className="card">
+            <h5 className="card-header">Category</h5>
+            <div className="table-responsive text-nowrap">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody className="table-border-bottom-0">
+                  {categories.map((category, index) => (
+                    <tr key={category.index}>
+                      <td>
+                        <strong>{category.id}</strong>
+                      </td>
+                      <td>{category.category_name}</td>
+                      {/* <td>{category.subject}</td>
+                      <td>{category.message}</td>
+                      <td>{category.contact_status}</td> */}
+                      <td>
+                        <div className="dropdown">
+                          <button
+                            type="button"
+                            className={`btn p-10 dropdown-toggle hide-arrow ${
+                              index === openedMenuIndex ? "show" : ""
+                            }`}
+                            data-bs-toggle="dropdown"
+                            onClick={() => toggleMenu(index)}
+                          >
+                            <i className="fa-solid fa-ellipsis-vertical"></i>
+                          </button>
+                          <div
+                            className={`dropdown-menu ${
+                              index === openedMenuIndex ? "show" : ""
+                            }`}
+                          >
+                            <a className="dropdown-item" href="#!">
+                              <i className="fa-solid fa-trash"></i>
+                              Delete
+                            </a>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ShowCategory;
