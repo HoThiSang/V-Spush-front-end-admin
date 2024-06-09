2eb1dcf23f394865d3ea7653a28fb307cc138c1c
HoMaiHuyen <huyen.homai25@student.passerellesnumeriques.org>
2024-06-09 13:49:24 +0700

    feat: Create new banner refer to ST 220

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {  Routes, Route, Navigate } from "react-router-dom";
-import { Order, Blog, CreateBlog, ShowContact, UpdateContact, UpdateBlog , Product,ShowCategory,DetailCategory,DeleteCategory} from "./pages";
+
+import { Order, Blog, CreateBlog, ShowContact, UpdateContact, UpdateBlog, Product, ShowCategory, DetailCategory, DeleteCategory, CreateBanner } from "./pages";
 import Login from "./pages/Login";
 import React from "react";
 import Banner from "./pages/Banner";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/create-blog" element={<CreateBlog />} />
         <Route path="/contacts" element={<ShowContact />} />
         <Route path="/banner" element={<Banner />} />
+        <Route path="/create-banner" element={<CreateBanner />} />
         <Route path="/update-contact/:id" element={<UpdateContact />} />
         <Route path="/update-blog/:id" element={<UpdateBlog />} />
         <Route path="/products" element={<Product />} />
--- src/pages/Blog/CreateBlog.js
+++ src/pages/Blog/CreateBlog.js
@@ -1,149 +1,149 @@
-// import React, { useState } from "react";
-// import { Flex, Input, Modal } from "antd";
-// import axiosService from "../../services/configAxios";
-// import "./style.css";
-// import { useForm } from "react-hook-form";
-// import AdminLayout from "../../layouts/AdminLayout";
-// const { TextArea } = Input;
+import React, { useState } from "react";
+import { Flex, Input, Modal } from "antd";
+import axiosService from "../../services/configAxios";
+import "./style.css";
+import { useForm } from "react-hook-form";
+import AdminLayout from "../../layouts/AdminLayout";
+const { TextArea } = Input;
 
-// const CreateBlog = () => {
-//   const [title, setTitle] = useState("");
-//   const [content, setContent] = useState("");
-//   const [imageFile, setImageFile] = useState(null);
-//   const [errorMessage, setErrorMessage] = useState("");
-//   const [successMessage, setSuccessMessage] = useState("");
-//   const [isErrorModalVisible, setIsErrorModalVisible] = useState(false);
-//   const [isSuccessModalVisible, setIsSuccessModalVisible] = useState(false);
-//   const [image, setImage] = useState(null);
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors }
-//   } = useForm();
+const CreateBlog = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [imageFile, setImageFile] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [isErrorModalVisible, setIsErrorModalVisible] = useState(false);
+  const [isSuccessModalVisible, setIsSuccessModalVisible] = useState(false);
+  const [image, setImage] = useState(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
 
-//   const handleChangeTitle = (e) => {
-//     setTitle(e.target.value);
-//   };
-//   const handleChangeContent = (e) => {
-//     setContent(e.target.value);
-//   };
-//   const handleImageUpload = (e) => {
-//     if (e.target.files && e.target.files[0]) {
-//       setImage(URL.createObjectURL(e.target.files[0]));
-//       setImageFile(e.target.files[0]);
-//     }
-//   };
+  const handleChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+  const handleImageUpload = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      setImage(URL.createObjectURL(e.target.files[0]));
+      setImageFile(e.target.files[0]);
+    }
+  };
 
-//   const handleFormSubmit = async (e) => {
-//     e.preventDefault();
-//     console.log("title : ", title);
-//     console.log("content : ", content);
-//     const formData = new FormData();
-//     formData.append("title", title);
-//     formData.append("content", content);
-//     formData.append("image_url", imageFile);
-//     try {
-//       const response = await axiosService.post("/admin-create-post", formData, {
-//         headers: {
-//           "Content-Type": "multipart/form-data"
-//         }
-//       });
-//       setTitle("");
-//       setContent("");
-//       setImageFile(null);
-//       setSuccessMessage("Blog created successfully!");
-//       setIsSuccessModalVisible(true);
-//     } catch (error) {
-//       if (
-//         error.response &&
-//         error.response.data &&
-//         error.response.data.message
-//       ) {
-//         setErrorMessage(error.response.data.message);
-//         setIsErrorModalVisible(true);
-//       } else {
-//         setErrorMessage("An error occurred. Please try again later.");
-//         setIsErrorModalVisible(true);
-//       }
-//     }
-//   };
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    console.log("title : ", title);
+    console.log("content : ", content);
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("content", content);
+    formData.append("image_url", imageFile);
+    try {
+      const response = await axiosService.post("/admin-create-post", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      });
+      setTitle("");
+      setContent("");
+      setImageFile(null);
+      setSuccessMessage("Blog created successfully!");
+      setIsSuccessModalVisible(true);
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setErrorMessage(error.response.data.message);
+        setIsErrorModalVisible(true);
+      } else {
+        setErrorMessage("An error occurred. Please try again later.");
+        setIsErrorModalVisible(true);
+      }
+    }
+  };
 
-//   return (
-//     <AdminLayout>
-//     <div className="content-wrapper">
-//       <div className="container-xxl flex-grow-1 container-p-y">
-//         <h4 className="fw-bold py-3 mb-4">
-//           <span className="text-muted fw-light">Forms/</span> Create new blog{" "}
-//         </h4>
-//         <form onSubmit={handleSubmit(handleFormSubmit)}>
-//           <div class="row">
-//             <div class="col-xl">
-//               <div class="card mb-4">
-//                 <div class="card-header d-flex justify-content-between align-items-center">
-//                   <h5 class="mb-0">Add new blog</h5>
-//                   <small class="text-muted float-end">l</small>
-//                 </div>
-//                 <div class="card-body">
-//                   <Flex vertical gap={32}>
-//                   {errors.title && <span className="error">{errors.title.message}</span>}
-//                     <TextArea
-//                       {...register("title", {
-//                         required: "Title is required",
-//                         maxLength: {
-//                           value: 100,
-//                           message: "Title must be less than 100 characters"
-//                         }
-//                       })}
-//                       showCount
-//                       maxLength={100}
-//                       placeholder="Enter blog title"
-//                       onChange={handleChangeTitle}
-//                       value={title}
-//                     />
-//                     <TextArea
-//                       showCount
-//                       maxLength={500}
-//                       placeholder="Enter blog content"
-//                       style={{ height: 120, resize: "none" }}
-//                       onChange={handleChangeContent}
-//                       value={content}
-//                     />
-//                     <input
-//                       type="file"
-//                       onChange={handleImageUpload}
-//                       accept="image/*"
-//                     />
-//                     <img src={image} alt="" className="image-fluid" />
-//                     <button className="btn btn-outline-primary" type="submit">
-//                       Submit
-//                     </button>
-//                   </Flex>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </form>
-//         <Modal
-//           className="error"
-//           title="Error"
-//           open={isErrorModalVisible}
-//           onOk={() => setIsErrorModalVisible(false)}
-//           onCancel={() => setIsErrorModalVisible(false)}
-//         >
-//           <p>{errorMessage}</p>
-//         </Modal>
-//         <Modal
-//           title="Success"
-//           open={isSuccessModalVisible}
-//           onOk={() => setIsSuccessModalVisible(false)}
-//           onCancel={() => setIsSuccessModalVisible(false)}
-//         >
-//           <p>{successMessage}</p>
-//         </Modal>
-//       </div>
-//     </div>
-//     </AdminLayout>
-//   );
-// };
+  return (
+    <AdminLayout>
+    <div className="content-wrapper">
+      <div className="container-xxl flex-grow-1 container-p-y">
+        <h4 className="fw-bold py-3 mb-4">
+          <span className="text-muted fw-light">Forms/</span> Create new blog{" "}
+        </h4>
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+          <div class="row">
+            <div class="col-xl">
+              <div class="card mb-4">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                  <h5 class="mb-0">Add new blog</h5>
+                  <small class="text-muted float-end">l</small>
+                </div>
+                <div class="card-body">
+                  <Flex vertical gap={32}>
+                  {errors.title && <span className="error">{errors.title.message}</span>}
+                    <TextArea
+                      {...register("title", {
+                        required: "Title is required",
+                        maxLength: {
+                          value: 100,
+                          message: "Title must be less than 100 characters"
+                        }
+                      })}
+                      showCount
+                      maxLength={100}
+                      placeholder="Enter blog title"
+                      onChange={handleChangeTitle}
+                      value={title}
+                    />
+                    <TextArea
+                      showCount
+                      maxLength={500}
+                      placeholder="Enter blog content"
+                      style={{ height: 120, resize: "none" }}
+                      onChange={handleChangeContent}
+                      value={content}
+                    />
+                    <input
+                      type="file"
+                      onChange={handleImageUpload}
+                      accept="image/*"
+                    />
+                    <img src={image} alt="" className="image-fluid" />
+                    <button className="btn btn-outline-primary" type="submit">
+                      Submit
+                    </button>
+                  </Flex>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+        <Modal
+          className="error"
+          title="Error"
+          open={isErrorModalVisible}
+          onOk={() => setIsErrorModalVisible(false)}
+          onCancel={() => setIsErrorModalVisible(false)}
+        >
+          <p>{errorMessage}</p>
+        </Modal>
+        <Modal
+          title="Success"
+          open={isSuccessModalVisible}
+          onOk={() => setIsSuccessModalVisible(false)}
+          onCancel={() => setIsSuccessModalVisible(false)}
+        >
+          <p>{successMessage}</p>
+        </Modal>
+      </div>
+    </div>
+    </AdminLayout>
+  );
+};
 
-// export default CreateBlog;
+export default CreateBlog;
