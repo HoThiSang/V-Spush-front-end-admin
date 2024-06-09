b88ff477a2d5490eee34cd22e0342883277d6968
Linh <nguyenthilinhqni2020@gmail.com>
2024-06-09 08:51:36 +0700

    feat: handle-api-create-category refer to 225

--- src/pages/Category/CreateCategory.js
+++ src/pages/Category/CreateCategory.js
@@ -3,32 +3,30 @@ import { useForm } from "react-hook-form";
 import axiosService from "../../services/configAxios";
 import { useNavigate } from "react-router";
 import AdminLayout from "../../layouts/AdminLayout";
-import Contact from "../../components/Contact";
 function CreateCategory() {
   const [category_name, setName] = useState("");
-  const [success, setSuccess] = useState("Let's create");
-
-  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const createCategory = async (e) => {
-    e.preventDefault();
+
+  const navigate = useNavigate();
+  const createCategory = async () => {
     try {
       const response = await axiosService.post(`/categories-create`, {
         category_name,
       });
-      setSuccess("Create category successfully");
+      navigate("/categories");
     } catch (error) {
       console.log("Error create category", error);
     }
   };
-  const getName = (e) => {
-    setName(e.target.value);
-    console.log(e.target.value);
-  };
+  const getName=(e)=>{
+    setName(e.target.value)
+    console.log(e.target.value)
+}
+
   return (
     <>
       <AdminLayout>
@@ -37,25 +35,45 @@ function CreateCategory() {
             <div className="col-lg-2"></div>
             <div className="col-lg-8 form-update-contact">
               <form onSubmit={handleSubmit(createCategory)}>
-                <Contact
-                  name ="Category name"
-                  value={category_name}
-                  title="Create Category"
-                  onChange={getName}
-                  {...register("message", {
-                    required: "Message is required",
-                    validate: (value) =>
-                      value.length > 10 ||
-                      "Message must be longer than 10 characters",
-                  })}
-                />
-<br />
+              <div className="row">
+                <div className="col-lg-2"></div>
+                <div className="col-lg-8">
+                  <br />
+                  <label htmlFor="message" className="object-contact">
+                    Message Reply
+                  </label>
+                  <br />
+                  <input
+                    {...register("message", {
+                      required: "Message is required",
+                      validate: (value) =>
+                        value.length > 10 ||
+                        "Message must be longer than 10 characters",
+                    })}
+                    type="text"
+                    id="message"
+                    placeholder="Enter category here"
+                    onChange={getName}
+                    className="input-update-contact"
+                  />
+                </div>
+                <br />
                 {errors.message && (
                   <p className="error-input">{errors.message.message}</p>
                 )}
-                <button type="submit">Create</button>
+
+                <button
+                  type="submit"
+                  className="btn btn-outline-primary input-update-contact"
+                >
+                  Submit
+                </button>
+                
+                <div className="col-lg-2"></div>
+              </div>
               </form>
             </div>
+            <div className="col-lg-2"></div>
           </div>
         </div>
       </AdminLayout>
